9f94f9949f56c4d0ef8150ecf9b8ec58feda302d
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-04-02 18:55:20 +0200

    create style for input

--- src/components/loginPageComponents/Input.jsx
+++ src/components/loginPageComponents/Input.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const InputField = styled.input`
+padding:10px 20px;
+border:0;
+margin-bottom:10px;
+width:300px;
+`
 
 const Input = (props) => {
     return (
-        <input type={props.type} placeholder={props.placeholder} />
+        <InputField type={props.type} placeholder={props.placeholder} />
     )
 }
 export default Input
