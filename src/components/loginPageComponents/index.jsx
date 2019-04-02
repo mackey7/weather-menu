91424e5593dffd8422a3b3aa7cd20cd798ef8d95
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-04-02 18:45:54 +0200

    add style for index

--- src/components/loginPageComponents/index.jsx
+++ src/components/loginPageComponents/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Header from './Header.jsx'
 import Form from './Form.jsx'
+import styled from 'styled-components'
+
+const Section = styled.section`
+display:flex;
+justify-content:center;
+flex-direction:column;
+align-items:center;
+background: rgb(10, 63, 161);
+min-width:100%;
+height:100vh;
+
+`;
 
 
 const LoginPageWrapper = () => {
     return (
-        <section>
+        <Section>
             <Header text="Login" />
             <Form />
-        </section>
+        </Section>
     )
 }
 export default LoginPageWrapper
