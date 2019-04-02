0b2bf1a588679ee4b02db1722cf5699b53090941
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-04-02 22:35:45 +0200

    create style

--- src/components/menuComponents/index.jsx
+++ src/components/menuComponents/index.jsx
@@ -2,14 +2,34 @@ import React from 'react';
 import MenuItem from './menuItem'
 import Header from './Header'
 import Footer from './Footer'
+import styled from 'styled-components'
 
+const MenuComponentWrapper = styled.div`
+    background: rgb(204, 211, 238);
+    height:100vh;
+    width:100%;
+    display:flex;
+    justify-content:center;
+`;
+
+const MenuContainer = styled.section`
+    display:flex;
+    flex-direction:column;
+    align-content: space-between;
+    margin:40px;
+    padding:20px;
+    width:80%;
+    background: rgb(238, 236, 236);
+`;
 const MenuComponent = () => {
     return (
-        <div>
-            <Header text="Menu" />
-            <MenuItem src="img" alt="zdjecie" title="nazwa produktu" price='12' />
-            <Footer/>
-        </div>
+        <MenuComponentWrapper>
+            <MenuContainer>
+                <Header text="Menu" />
+                <MenuItem src="img" alt="zdjecie" title="nazwa produktu" price='12' />
+                <Footer />
+            </MenuContainer>
+        </MenuComponentWrapper>
     )
 }
 export default MenuComponent;
