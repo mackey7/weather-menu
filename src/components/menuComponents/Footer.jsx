cd17d0fb544be2c170eee5b5ff6000f5234c3a82
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-04-02 22:17:28 +0200

    create footer component

---
+++ src/components/menuComponents/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <i className="fas fa-user-cog"></i>
+        </footer>
+    )
+}
+export default Footer;
--- src/components/menuComponents/index.jsx
+++ src/components/menuComponents/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MenuItem from './menuItem'
 import Header from './Header'
+import Footer from './Footer'
 
 const MenuComponent = () => {
     return (
         <div>
             <Header text="Menu" />
             <MenuItem src="img" alt="zdjecie" title="nazwa produktu" price='12' />
+            <Footer/>
         </div>
     )
 }
