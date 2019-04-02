075a9a7d7770009bd52a9442f1b6669ff1025c1c
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-04-02 18:49:57 +0200

    create style for form

--- src/components/loginPageComponents/Form.jsx
+++ src/components/loginPageComponents/Form.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Input from './Input.jsx';
 import Button from './Button.jsx'
+import styled from 'styled-components'
+
+const FormArea = styled.form`
+    background: rgba(246, 246, 246, 0.287);
+    padding:10px;
+    display:flex;
+    flex-direction:column;
+`;
+
+
 const Form = () => {
     return (
-        <form action="">
+        <FormArea action="">
             <Input type="text" placeholder="e-mail" />
             <Input type="password" placeholder="password" />
             <Button value="Log In" />
-        </form>
+        </FormArea>
     )
 }
 export default Form
