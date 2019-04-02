0b333f82d30642dcfdcbaac708cd8574b935e5db
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-04-02 20:14:03 +0200

    create structure for menu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import LoginPageContainer from './containers/loginPageContainer'
+import MenuComponent from './components/menuComponents'
 class App extends Component {
   render() {
     return (
       <div >
-        < LoginPageContainer />
+        <MenuComponent />
+        {/* < LoginPageContainer /> */}
       </div>
     );
   }
---
+++ src/components/menuComponents/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuItem from './menuItem'
+import Header from './Header'
+
+const MenuComponent = () => {
+    return (
+        <div>
+            <Header />
+            <MenuItem />
+        </div>
+    )
+}
+export default MenuComponent;
